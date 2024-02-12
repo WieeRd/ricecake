20919bb971bd323745285d8c640c1d8a6593b181
WieeRd <verywieerd@gmail.com>
2024-02-12 22:29:42 +0900

    docs: document `hangul.convert` module

--- ricecake/hangul/convert.py
+++ ricecake/hangul/convert.py
@@ -1,6 +1,6 @@
-"""Conversion between Hangul Jamo and Compatibility Jamo."""
+"""Conversion between Hangul Jamo and Hangul Compatibility Jamo."""
 
-from .offset import MODERN_JUNGSEONG_BASE, modern_compat_moum_offset
+# from .offset import *
 
 __all__ = [
     "jamo_to_compat_jamo",
@@ -10,22 +10,42 @@ __all__ = [
 ]
 
 
+# FIX: all *modern* Jamo can be mapped to Compat Jamo
 def jamo_to_compat_jamo(c: str, /) -> str | None:
-    """Converts a Jamo character to a Compatibility Jamo character."""
+    """Converts a Hangul Jamo character to a Compatibility Jamo character.
+
+    Returns `None` if there is no corresponding Compatibility Jamo character.
+
+    Raises:
+        ValueError: If the character is not a Hangul Jamo.
+    """
     raise NotImplementedError
 
 
 def compat_jaum_to_choseong(c: str, /) -> str | None:
-    """Converts a Compatibility Jaum character to a Jamo Choseong character."""
+    """Converts a Hangul Compatibility Jaum character to a Jamo Choseong character.
+
+    Returns `None` if there is no corresponding Jamo Choseong character.
+
+    Raises:
+        ValueError: If the character is not a Hangul Compatibility Jamo Jaum.
+    """
     raise NotImplementedError
 
 
 def compat_moum_to_jungseong(c: str, /) -> str:
-    """Converts a Compatibility Moum character to a Jamo Jungseong character."""
-    offset = modern_compat_moum_offset(c)
-    return chr(offset + MODERN_JUNGSEONG_BASE)
+    """Converts a Hangul Compatibility Moum character to a Jamo Jungseong character.
+
+    Raises:
+        ValueError: If the character is not a Hangul Compatibility Jamo Moum.
+    """
+    raise NotImplementedError
 
 
 def compat_jaum_to_jongseong(c: str, /) -> str:
-    """Converts a Compatibility Jaum character to a Jamo Jongseong character."""
+    """Converts a Hangul Compatibility Jaum character to a Jamo Jongseong character.
+
+    Raises:
+        ValueError: If the character is not a Hangul Compatibility Jamo Jaum.
+    """
     raise NotImplementedError
