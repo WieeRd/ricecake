a83da91ede1b5dc755f50b6fded42dda9d728c1d
WieeRd <verywieerd@gmail.com>
2024-02-17 21:23:43 +0900

    feat: implement `decompose()`

--- ricecake/hangul/compose.py
+++ ricecake/hangul/compose.py
@@ -1,10 +1,14 @@
 """(De)composition of Hangul Jamo and Syllable."""
 
-from . import offset
+from . import offset as o
 
 __all__ = ["compose", "decompose"]
 
 
+# FEAT: LATER: compose syllable compat jamo
+# | - provide separate function
+# | - boolean flag parameter
+# | - normalize the input every time
 def compose(cho: str, jung: str, jong: str | None) -> str:
     """Composes Choseong, Jungseong, and an optional Jongseong into a Syllable.
 
@@ -12,16 +16,30 @@ def compose(cho: str, jung: str, jong: str | None) -> str:
         ValueError: If the characters are not appropriate Hangul Jamos.
     """
     return chr(
-        offset.modern_choseong_offset(cho) * offset.CHOSEONG_COEF
-        + offset.modern_jongseong_offset(jung) * offset.JUNGSEONG_COEF
-        + (offset.modern_jongseong_offset(jong) if jong else 0)
-        + offset.SYLLABLE_BASE
+        o.modern_choseong_offset(cho) * o.CHOSEONG_COEF
+        + o.modern_jongseong_offset(jung) * o.JUNGSEONG_COEF
+        + (o.modern_jongseong_offset(jong) if jong else 0)
+        + o.SYLLABLE_BASE
     )
 
 
-def decompose(syl: str) -> tuple[str, str, str | None]:
-    """Decomposes a Syllable into Choseong, Jungseong, and Jongseong."""
-    raise NotImplementedError
+def decompose(c: str, /) -> tuple[str, str, str | None]:
+    """Decomposes a Syllable into Choseong, Jungseong, and an optional Jongseong.
+
+    Raises:
+        ValueError: If the character is not a Hangul Syllable.
+    """
+    syl = o.syllable_offset(c)
+
+    cho = syl // o.CHOSEONG_COEF
+    jung = syl % (o.CHOSEONG_COEF) // o.JUNGSEONG_COEF
+    jong = syl % o.JUNGSEONG_COEF
+
+    return (
+        chr(cho + o.MODERN_CHOSEONG_BASE),
+        chr(jung + o.MODERN_JUNGSEONG_BASE),
+        chr(jong + o.MODERN_JONGSEONG_BASE - 1) if jong else None,
+    )
 
 
 # FEAT: decompose composite Jaum and Moum into tuple of str
